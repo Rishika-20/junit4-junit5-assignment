4bbfe9efc8a2c07c76b26c0e946e4b74521440ed
Rishika0101 <rishika.kumari@knoldus.com>
2023-04-12 23:54:44 +0530

    Updates as per suggested Pr comments

--- JUnit5Testing_final/src/test/java/PersonTest.java
+++ JUnit5Testing_final/src/test/java/PersonTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,31 +13,36 @@ public class PersonTest {
 
     //Test cases: for name.
     @Test
-    public void nameTest_non_empty_string(){
+    @DisplayName("Testing name with valid Input")
+    public void nameTest_givenNonEmptyInput_shouldReturnName(){
 
         person.setName("Rishika");
         assertEquals("Rishika", person.getName());
     }
     @Test
-    public void nameTest_empty_string() {
+    @DisplayName("Testing name with empty Input")
+    public void nameTest_givenEmptyInput_shouldReturnNull() {
 
         person.setName("");
         assertEquals("", person.getName());
     }
     //Test cases: for age.
     @Test
-    public void ageTest_positive_value(){
+    @DisplayName("Testing age with positive Input")
+    public void ageTest_givenPositiveValue_shouldReturnValidAge(){
         person.setAge(32);
         assertEquals(32, person.getAge());
     }
     @Test
-    public void ageTest_negative_value() {
+    @DisplayName("Testing age with negative Input")
+    public void ageTest_givenNegativeInput_shouldReturnNull() {
         person.setAge(0);
         assertTrue(person.getAge() <= 0, "Reminder! age cannot be -ve or 0.");
     }
     //Test cases: for Emails.
     @Test
-    public void emailTest_with_validation(){
+    @DisplayName("Testing email validation")
+    public void emailTest_givenInputEmail_shouldReturnValidEmail(){
         person.setEmail("Rishika@gmail.com");
         assertEquals("Rishika@gmail.com", person.getEmail());
         String email = person.getEmail();
